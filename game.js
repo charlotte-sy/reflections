1d27ba9cce50b043b59c2c0993cbbf3c59b837b3
seoyeon <sypark.charlotte@gmail.com>
2017-11-08 20:48:35 +0900

    yellow

--- game.js
+++ game.js
@@ -709,6 +709,7 @@ Coin = function () {
               0, -5]);
 
   this.collidesWith = ["ship"];
+  this.color = 'yellow';
 
   this.newPosition = function () {
     this.x = Math.random() * Game.canvasWidth;
